1cefd5454aa89f8eb941b3fc366b4a5ca2b2a083
Amr Youssef <amr.youssef@affirm.com>
2023-09-16 21:19:50 -0700

    Set default EDITOR and alias k=kubectl

--- install.sh
+++ install.sh
@@ -3,3 +3,12 @@
 # I use Kitty, and this does away with some annoying errors I keep getting
 # due to xterm-kitty terminfo missing by default.
 sudo apt-get install kitty-terminfo
+
+# Append is safer than override in case Thors start using .bashrc for its own setup logic
+cat <<EOL >> ~/.bashrc
+# Amr's custom stuff from here on, added by amr/dotfiles/install.sh
+
+alias k=kubectl
+# no na no
+export EDITOR=vim
+EOL
